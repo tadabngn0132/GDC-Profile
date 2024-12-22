4f123a5f73f311f637fa6f44c6a92cce02408b04
Nguyen Ba Dat <datngba2310@gmail.com>
2024-12-22 13:03:42 +0700

    Update

--- js/home.js
+++ js/home.js
@@ -8,3 +8,54 @@ document.addEventListener('click', () => {
         navMenu.classList.remove('show')
     }
 });
+
+// Xử lý image slider
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengthItem = items.length - 1;
+
+next.onclick = function() {
+    if (active + 1 > lengthItem) {
+        active = 0;
+    } else {
+        active = active + 1;
+    }
+    reloadSlider();
+}
+
+prev.onclick = function() {
+    if (active - 1 < 0) {
+        active = lengthItem;
+    } else {
+        active = active - 1;
+    }
+    reloadSlider();
+}
+
+let refreshSlider = setInterval(() => {next.click()}, 4000);
+
+function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+
+    dots[active].classList.add('active');
+
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(() => {next.click()}, 4000);
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function() {
+        active = key;
+        reloadSlider();
+    })
+})
+
